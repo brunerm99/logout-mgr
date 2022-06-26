ca2be7680ce140122835ee83f58b99e7df256dc3
Marshall Bruner <brunerm99@gmail.com>
2022-06-27 00:11:08 +0200

    Add GTK and create window

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import qualified GI.Gtk as Gtk
 
 main :: IO ()
-main = someFunc
+main = do
+  _ <- Gtk.init Nothing
+
+  win <- Gtk.windowNew Gtk.WindowTypeToplevel
+  Gtk.setContainerBorderWidth win 10
+  Gtk.setWindowTitle win "Logout Manager"
+  Gtk.setWindowResizable win False
+  Gtk.setWindowDefaultWidth win 750
+  Gtk.setWindowDefaultHeight win 250
+  Gtk.setWindowWindowPosition win Gtk.WindowPositionCenter
+  Gtk.windowSetDecorated win False
+
+  _ <- Gtk.onWidgetDestroy win Gtk.mainQuit
+
+  #showAll win
+
+  Gtk.main
